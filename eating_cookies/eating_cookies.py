74c273cd0b99d8abb660e15c367ffbd226a0a2a6
PeteDram <me@petejames.com>
2020-10-06 20:51:41 -0700

    eating cookies naive solution complete but it hands on the big test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,10 +5,25 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    pass
+    #error handling
+    if n < 1:
+        return 1
+
+    #base case
+    if n == 1:
+        return 1 
+    if n == 2:
+        return 2 
+    if n == 3:
+        return 4 
+
+    #recursion
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 10
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
